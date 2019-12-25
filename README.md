b6f46e8e0cd18fc5fa1825b9948389baf69ea0cb
huangwanneng2 <china.heath.hwn@gmail.com>
2019-12-25 18:28:49 +0800

    提交代码

---
+++ README.md
@@ -0,0 +1,37 @@
+# h5单页开发的模板
+本模板配合 `varied-cli` 使用，利用 `varied-cli` 快速构建h5单页开发工程。
+
+### 安装
+
+```
+npm install varied-cli -g
+```
+### 构建方法
+
+```
+varied init webpack-simple mytest
+```
+
+
+## 项目技术栈
+
+| 构成         | 框架    |
+| ------------ | ------- |
+| 前端视图页面 | vue     |
+| 工程化       | webpack |
+
+## 项目本地运行
+
+```
+npm install
+npm run start
+```
+
+## 项目构建
+
+```
+// 开发环境构建
+npm run dev
+// 生产环境构建
+npm run build
+```
---
+++ template/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/varied.png">
+    <h1>\{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style{{#less}} lang="less"{{/less}}>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
